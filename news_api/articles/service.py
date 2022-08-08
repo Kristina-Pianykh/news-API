94da86c00cf2e9191e94bee992b33a6d9cd39926
Kristina Pianykh <kristinavrnrus@gmail.com>
2022-08-08 07:47:58 +0200

    chore: improve the model performance

--- news_api/articles/service.py
+++ news_api/articles/service.py
@@ -1,4 +1,42 @@
-def get():
+from datetime import datetime
+from typing import Any, MutableMapping, Union
+from uuid import UUID
+
+from bson.errors import InvalidId
+from bson.objectid import ObjectId
+from pymongo import MongoClient
+
+from .models import Article
+
+client = MongoClient("mongodb://127.0.0.1:27017")
+db = client.local
+news = db.news
+
+Document = MutableMapping[
+    str,
+    Union[ObjectId, UUID, datetime, str],
+]
+
+# client.close() ?
+
+
+def get(query_param: str) -> Union[Article, None]:  # by id, uuid, title
+    filter_by: Document = {}
+    try:
+        filter_by["_id"] = ObjectId(query_param)
+    except InvalidId:
+        filter_by["uuid"] = UUID(query_param)
+    except ValueError:
+        filter_by["title"] = query_param
+
+    result: Union[MutableMapping, None] = news.find_one(filter_by)
+    try:
+        return Article(**result)
+    except TypeError:
+        return None
+
+
+def get_many():  # by author, date
     raise NotImplementedError
 
 
@@ -10,9 +48,7 @@ def delete():
     raise NotImplementedError
 
 
-def create():
-    raise NotImplementedError
-
-
-def get_many():
-    raise NotImplementedError
+def create(input_data: dict[str, Any]) -> ObjectId:
+    article = Article(**input_data)
+    result = news.insert_one(article.dict())
+    return result.inserted_id
